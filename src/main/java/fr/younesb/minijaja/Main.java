e38021508dc285e9f23a2c9c47f5aea17c906fd0
YounesB <younes.benhamroura@gmail.com>
2026-09-16 21:15:25 +0200

    chore: add Maven skeleton with Java 21 and JUnit 5

---
+++ src/main/java/fr/younesb/minijaja/Main.java
@@ -0,0 +1,10 @@
+package fr.younesb.minijaja;
+
+public final class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.exit(64);
+        }
+        System.out.println("MiniJaja 0.1.0");
+    }
+}
---
+++ src/test/java/fr/younesb/minijaja/SmokeTest.java
@@ -0,0 +1,11 @@
+package fr.younesb.minijaja;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+    @Test
+    void smokeTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
